1ec2895865df91a9080d0df325ccf0febe3596d9
Strahinja Stefanović <stefanovic.strahinja97@gmail.com>
2022-01-18 23:07:34 +0100

    data cleaning update

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,41 +1,56 @@
-import pandas as pd
 import glob
-
-comments_path = "dataset/reddit_comments_2008"
-submissions_path = "dataset/reddit_submissions_2008"
+import pandas as pd
 
 
 def read_data(directory):
-    all_files = glob.glob(directory + "/*.csv")
-    frames = []
+    data_files = glob.glob(directory + "/*.csv")
+    data_frames = []
 
-    for month, filename in enumerate(all_files):
-        frame = pd.read_csv(filename, index_col=0, low_memory=False)
-        frame["month"] = month + 1
-        frames.append(frame)
+    for month, filename in enumerate(data_files):
+        data_frame = pd.read_csv(filename, index_col=0, low_memory=False)
+        data_frame["month"] = month + 1
+        data_frames.append(data_frame)
 
-    return pd.concat(frames, ignore_index=True)
-
-
-def clean_submissions(submissions):
-    print(submissions["id"].isnull().values.any())
-    print(submissions["id"].is_unique)
-
-    submissions = submissions.drop(["created_utc", "url", "permalink", "domain", "distinguished"], axis=1)
-
-    print(submissions)
-    print(submissions.shape)
-
-    submissions = submissions[submissions["author"] != "[deleted]"]
-
-    submissions = submissions.rename(columns={"id": "submission_id"})
-    submissions["id"] = submissions.index
-
-    return submissions
+    return pd.concat(data_frames, ignore_index=True)
 
 
-submissions_df = read_data(submissions_path)
-submissions_df = clean_submissions(submissions_df)
+def clean_data(data_frame, columns_to_drop, name_id):
 
-comments_df = read_data(comments_path)
+    data_frame = data_frame[data_frame["id"].notnull()]
+    data_frame = data_frame.drop(columns_to_drop, axis=1)
+    data_frame = data_frame[data_frame["author"] != "[deleted]"]
+    data_frame = data_frame.rename(columns={"id": name_id})
 
+    data_frame.reset_index(drop=True, inplace=True)
+    data_frame["id"] = data_frame.index
+
+    print(data_frame.dtypes)
+    print(data_frame)
+    print(data_frame.shape)
+
+    return data_frame
+
+
+def export_cleaned_data(data_frame, filename):
+    data_frame.to_csv(f"dataset/cleaned/{filename}.csv", index=False)
+    data_frame.to_pickle(f"dataset/cleaned/{filename}")
+
+
+def prepare_data():
+    submissions_path = "dataset/reddit_submissions_2008"
+    comments_path = "dataset/reddit_comments_2008"
+
+    submissions_columns = ["created_utc", "url", "permalink", "domain", "distinguished"]
+    comments_columns = ["created_utc", "distinguished", "controversiality"]
+
+    submissions = read_data(submissions_path)
+    submissions = clean_data(submissions, submissions_columns, "submission_id")
+
+    comments = read_data(comments_path)
+    comments = clean_data(comments, comments_columns, "comment_id")
+
+    export_cleaned_data(submissions, "submissions")
+    export_cleaned_data(comments, "comments")
+
+
+prepare_data()
