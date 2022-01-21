751e243536559f6ed7885d7db50cf0424d519b72
Ana Stakic <anastakic26@gmail.com>
2022-01-21 23:12:57 +0100

    Added SNetF and SNetF

--- network_modeling.py
+++ network_modeling.py
@@ -1,7 +1,6 @@
 # !import networkx as nx
 import networkx as nx
-
-# !import pandas as pd
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -40,6 +39,37 @@ def subreddits_per_user(graph, *data_frames):
     return result
 
 
+def edge_weight_visualization(graph, threshold):
+
+    edge_weight = [int(graph.edges[edge]['weight']) for edge in SNet.edges]
+
+    edge_weight_keys = list(set(edge_weight))
+    edge_weight_keys.sort()
+    edge_weight_values = list(map(lambda edge: edge_weight.count(edge), edge_weight_keys))
+
+    plt.bar(edge_weight_keys[w_threshold:], edge_weight_values[w_threshold:])
+    plt.xlabel('Edge Weight')
+    plt.ylabel('Edge Num')
+    plt.show()
+
+
+def generate_snet_filtered(graph, threshold):
+    to_remove = [(a, b) for a, b, attrs in graph.edges(data=True) if attrs['weight'] <= threshold]
+    # to_remove = [edge for edge in graph.edges(data=True) if edge["weight"] <= threshold]
+    graph.remove_edges_from(to_remove)
+    return graph
+
+
+def generate_snet_target(graph, nodes):
+    nodes_to_remove = [node for node in graph.nodes if node not in nodes]
+    graph.remove_nodes_from(nodes_to_remove)
+    new_graph = nx.Graph()
+    new_graph.add_nodes_from(nodes)
+    for a, b, attrs in graph.edges(data=True):
+        new_graph.add_edge(a, b, weight=attrs['weight'])
+    return new_graph
+
+
 submissions = pd.read_pickle("dataset/cleaned/submissions")
 comments = pd.read_pickle("dataset/cleaned/comments")
 
@@ -51,7 +81,23 @@ SNet.add_nodes_from(subreddits)
 
 subreddit_user_map = subreddits_per_user(SNet, submissions, comments)
 
-for key in subreddit_user_map:
-    print(len(subreddit_user_map[key]))
-
 nx.write_pajek(SNet, "dataset/models/snet.net")
+
+
+w_threshold = 3
+SNet = nx.Graph(nx.read_pajek("dataset/models/snet.net"))
+
+edge_weight_visualization(SNet, w_threshold)
+SNetF = generate_snet_filtered(SNet, w_threshold)
+nx.write_pajek(SNetF, "dataset/models/snetf.net")
+
+subreddits_filter = ['reddit.com', 'pics', 'worldnews', 'programming', 'math',
+                    'business', 'politics', 'obama', 'science', 'technology',
+                    'WTF', 'AskReddit', 'netsec', 'philosophy', 'videos', 'offbeat',
+                    'funny', 'entertainment', 'linux', 'geek', 'gaming', 'comics',
+                    'gadgets', 'nsfw', 'news', 'environment', 'atheism', 'canada',
+                    'Economics', 'scifi', 'bestof', 'cogsci', 'joel', 'Health',
+                    'guns', 'photography', 'software', 'history', 'ideas']
+
+SNetT = generate_snet_target(SNet, subreddits_filter)
+nx.write_pajek(SNetT, "dataset/models/snett.net")
